e35506778d52d3817d0ff210f061b0860bfa5805
frankmusiime <f.musiime@alustudent.com>
2025-03-06 16:02:20 -0800

    my script

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,6 +20,26 @@ class Square:
 
         self.__size = size
 
+    @property
+    def size(self) -> int:
+        """
+        Retrieve the instance attribute size
+        :return: the size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value: int) -> None:
+        """
+        Set the value of the size
+        :param: int size
+        """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self) -> int:
         """
         Calculates and returns the area of the square
